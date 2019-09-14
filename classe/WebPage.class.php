1b649a5e13bb3504ad00ee6ff9d4444e12180981
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2019-09-14 11:37:57 +0200

    Exploration du DOM fini

---
+++ classe/WebPage.class.php
@@ -0,0 +1,101 @@
+<?PHP
+
+class WebPage 
+{
+    private $head; #string
+    private $title;#string
+    private $body; #string
+
+    
+    public function __construct(string $title=null)
+    {
+        $this->body="";
+        $this->title=$title;
+        $this->head="";
+    }
+    
+    public function body():string
+    {
+        return $this->body;
+    }
+    
+    public function head():string
+    {
+        return $this->head;
+    }
+
+    public function setTitle(string $title):void
+    {
+        $this->title=$title;
+    }
+
+    public function appendToHead(string $content):void
+    {
+        $this->head.=$content;
+    }
+
+    public function appendCss(string $css):void
+    {
+        $this->head.="<style>$css</style>";
+    }
+
+    public function appendCssUrl(string $url):void
+    {
+        $this->head.='<link href="'.$url.'" rel="stylesheet" type="text/css"/>';
+    }
+
+    public function appendJs(string $js):void
+    {
+        $this->head.='<script type="text/javascript">'.$js.'</script>';
+    }
+
+    public function appendJsUrl(string $url):void
+    {
+        $this->head.='<script type="text/javascript" src="'.$url.'"></script>';
+    }
+
+    public function appendContent(string $content):void
+    {
+        $this->body.=$content;
+    }
+
+  public function getLastModification():string
+    {
+        $lastModification=strftime('%D',getlastmod());
+        return "<span>$lastModification</span>";
+    }    
+
+    public static function escapeString(string $string):string
+    {
+        return htmlspecialchars($string, ENT_QUOTES | ENT_HTML5, "utf-8");
+    }
+
+    public function toHTML():string
+    {
+        $date=$this->getLastModification();
+        if(is_null($this->title))
+        {
+           throw new exception ('Pas de titre') ;
+        }
+        else
+        {
+            
+            $html=<<<html
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+                <title>$this->title</title>
+                $this->head
+            </head>
+            <body>
+                $this->body
+                <footer>Dernière modification de cette page le $date</footer>
+            </body>
+html;
+            return $html;
+        }      
+        
+    }    
+
+  
+
+}
---
+++ explorationDOM.php
@@ -0,0 +1,17 @@
+<?php
+require_once "./classe/WebPage.class.php";
+$xml = new DOMdocument();
+$xml->load("http://www.bfmtv.com/rss/societe/");
+$html = new WebPage("voir bfm");
+$items = $xml->getElementsByTagName("item");
+$titres = $xml->getElementsByTagName("title");
+$titre = $titres[0]->nodeValue;
+
+$html->appendContent("<h1>Les nouvelles de '$titre'</h1>");
+foreach($items as $item){
+    $titre = $item->getElementsByTagName("title")[0]->nodeValue;
+    $html->appendContent("<p>".$titre."</p>");
+    
+}
+
+echo $html->toHTML();
